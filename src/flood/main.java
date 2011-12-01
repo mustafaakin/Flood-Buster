3bd0bd38c993b75f8b5ba3052bcdbeb6bd81864d
Mustafa <Mustafa@139.179.206.215>
2011-12-01 21:31:38 +0200

    First commit

---
+++ src/flood/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flood;
+
+/**
+ *
+ * @author Mustafa
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
